3b39bf1cc71a6d02a6beb70b51c7db2bfeb6073c
EKracht <ethan1kracht@gmail.com>
2015-11-01 21:47:27 -0800

    This git will make my calc pretty and prevent errors

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
 'use strict'
 
 var clickedNumbers = "";
+var buttonVal = "";
 var previousVal = 0;
 var currentVal = 0;
 
 $(document).ready(init);
 
-function init(){
+function init() {
   $('.number').click(showNumbers);
   $('.operator2').click(showOperators);
-  $('.operator1').click(showOperators);
   $('#equals').click(equals);
   $('#clear').click(clear);
+  $('#plusMinus').click(plusMinus);
+  $('#percent').click(doPercent);
 }
 
 function showNumbers() {
-    var buttonVal = this.id;
+  var str = clickedNumbers.toString();
+  var dec = str.lastIndexOf(".");
+  var buttonVal = this.id;
+  if (dec == -1) {
     clickedNumbers += buttonVal;
     $("#numArea").text(clickedNumbers);
+    return;
+  }
+  if (dec != -1 && buttonVal != ".") {
+    clickedNumbers += buttonVal;
+    $("#numArea").text(clickedNumbers);
+    return;
+  }
 }
 
 function showOperators() {
-    var buttonVal = this.value;
-    if (previousVal > 0 || previousVal < 0) {
-      clickedNumbers += previousVal + buttonVal;
-    } else {
-      clickedNumbers += buttonVal;
-    }
-    $("#numArea").text(clickedNumbers);
+  var buttonVal = this.value;
+  clickedNumbers += buttonVal;
+  $("#numArea").text(clickedNumbers);
 }
 
 function equals() {
-  var result = equalsHelper(clickedNumbers);
-  $('#answer').text(result);
-  $('#numArea').text(result);
-  clickedNumbers = result;
+  currentVal = equalsHelper(clickedNumbers);
+  $('#answer').text(currentVal);
+  $('#numArea').text(currentVal);
+  clickedNumbers = currentVal;
   buttonVal = "";
 }
 
 function equalsHelper(str){
-
   var o1 = str.lastIndexOf("+");
   var o2 = str.lastIndexOf("-");
   var o3 = str.lastIndexOf("*");
   var o4 = str.lastIndexOf("/");
-  var o5 = str.lastIndexOf("%");
-  var o6 = str.lastIndexOf("inv");
-  var o7 = str.lastIndexOf("^");
+  var o5 = str.lastIndexOf("^");
 
-  var maxOp = Math.max(o1, o2, o3, o4, o5, o6, o7);
+  var maxOp = Math.max(o1, o2, o3, o4, o5);
 
   if (maxOp == o1 && str[str.lastIndexOf("+") + 1]) {
     var index = str.lastIndexOf("+");
@@ -82,26 +87,7 @@ function equalsHelper(str){
       return +equalsHelper(frontPart) / +lastNum;
     }
   }
-  // if (str.indexOf("/") > 0 && str[str.indexOf("/") + 1] == 0) {
-  //   $("#answer").text("Error");
-  // }
-  if (maxOp == o5) {
-    var index = str.lastIndexOf("%");
-    var frontPart = str.substring(0, index);
-    var lastNum = str.substring(index + 1);
-    if (frontPart) {
-      return +equalsHelper(frontPart) / 100;
-    }
-  }
-  if (maxOp == o6) {
-    var index = str.lastIndexOf("inv");
-    var frontPart = str.substring(0, index);
-    var lastNum = str.substring(index + 1);
-    if (frontPart) {
-      return +equalsHelper(frontPart) * (-1);
-    }
-  }
-  if (maxOp == o7 && str[str.lastIndexOf("^") + 1] > 1) {
+  if (maxOp == o5 && str[str.lastIndexOf("^") + 1] > 1) {
     var index = str.lastIndexOf("^");
     var frontPart = str.substring(0, index);
     var lastNum = str.substring(index + 1);
@@ -109,10 +95,31 @@ function equalsHelper(str){
       return Math.pow(equalsHelper(frontPart), lastNum);
     }
   } 
-  // previousVal = $('#answer').text();
-  // $("#numArea").text(previousVal);
-  // clickedNumbers = $("#numArea").val();
   return +str;
+}
+
+function plusMinus() {
+  var str = clickedNumbers.toString();
+  if (str.lastIndexOf("+") != -1 || str.lastIndexOf("-") > 0 || str.lastIndexOf("*") != -1 || str.lastIndexOf("/") != -1 || str.lastIndexOf("^") != -1) {
+    return;
+  }
+  currentVal = invNum(clickedNumbers);
+  $('#answer').text(currentVal);
+  $('#numArea').text(currentVal);
+  clickedNumbers = currentVal;
+  buttonVal = "";
+}
+
+function doPercent() {
+  var str = clickedNumbers.toString();
+  if (str.lastIndexOf("+") != -1 || str.lastIndexOf("-") != -1 || str.lastIndexOf("*") != -1 || str.lastIndexOf("/") != -1 || str.lastIndexOf("^") != -1) {
+    return;
+  }
+  currentVal = percent(clickedNumbers);
+  $('#answer').text(currentVal);
+  $('#numArea').text(currentVal);
+  clickedNumbers = currentVal;
+  buttonVal = "";
 }
 
 function clear() {
@@ -124,37 +131,30 @@ function clear() {
   previousVal = 0;
 }
 
-
-function sum(a, b){
-  return +a + +b;
-}
-
-function minus(a, b){
-  return +a - +b;
-}
-
-function multiply(a, b){
-  return +a * +b;
-}
-
-function divide(a, b){
-  return +a / +b;
-}
-
 function percent(a){
   return +a / 100;
 }
 
-function plusMinus(a){
-  return a * (-1);
+function invNum(a){
+  return +a * (-1);
 }
 
-function expNum(a, b){
-  return Math.pow(a, b);
-}
+// function sum(a, b){
+//   return +a + +b;
+// }
 
+// function minus(a, b){
+//   return +a - +b;
+// }
 
-    // if (index == -1) {
-    // console.log('inhit');
-    //   return +str;
-    // }
+// function multiply(a, b){
+//   return +a * +b;
+// }
+
+// function divide(a, b){
+//   return +a / +b;
+// }
+
+// function expNum(a, b){
+//   return Math.pow(a, b);
+// }
